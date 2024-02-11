668a0e36e02093ce0163d2eaac24bfbfdd4cf1c8
byeolyi shin <mainichi_@naver.com>
2024-02-11 23:12:38 +0900

    랜덤게임만들기

---
+++ main.js
@@ -0,0 +1,169 @@
+/**
+ * 1.랜덤번호 지정 
+ * 2. 유저가 번호를 입력한다 . 그리고 go라는 버튼을 누름 
+ * 3.만약 유저가 랜덤 번호를 맞추면 "맞췄습니다!"
+ * 4.랜덤 번호 < 유저번호 "Down!
+ * 5.랜덤 번호 > 유저번호 "Up!"
+ * 6.버튼을 누르면 게임이 리셋된다 
+ * 7.5번의 기회를 다 쓰면 게임이 끝남 (더 이상 추측 불가 , 버튼이 Disabled)
+ * 8.유저가 1~100 범위 밖에 숫자를 입력하면 알려준다, 기회를 깎지 않는다 . 
+ * 9.유저가 이미 입력한 숫자를 또 입력하면 알려준다, 기회를 깎지 않는다. 
+ */
+
+
+//(4트)
+let randomNumber = 0;
+
+let resultArea = document.getElementById("result-area");
+let userNumber = document.getElementById("number-area");
+let buttonGo = document.getElementById("btn-go");
+
+buttonGo.addEventListener("click", play)
+
+function randomGame (){
+  randomNumber = Math.floor(Math.random()*100)+1;
+  console.log("정답은",randomNumber);
+}
+
+randomGame ();
+
+function play(){
+  let userValue = userNumber.value;
+  // console.log("유저가 고른 값",userValue)
+  
+  if(userValue > randomNumber){
+    resultArea.textContent = "다운";
+    // console.log("down")
+  } else if(userValue < randomNumber){
+    resultArea.textContent = "업";
+    // console.log("up")
+  } else {
+    resultArea.textContent = "정답";
+    // console.log("정답")
+  }
+}
+play()
+
+
+
+
+
+
+
+// (3트)
+// let randomNumber = 0 
+
+// let resultArea = document.getElementById("result-area") ;
+// let userNumber = document.getElementById("number-area") ;
+// let buttonGo = document.getElementById("btn-go") ;
+// //2.유저가 입력한 값 불러오기 
+// buttonGo.addEventListener("click",play)
+
+// function randomGame(){
+//   randomNumber = Math.floor(Math.random()*100)+1;
+//   console.log("정답은",randomNumber)
+// }
+
+// randomGame() //1.함수호ㅜㅊ출 
+
+// function play(){
+//   let userValue = userNumber.value
+//   console.log(userValue)
+//   if(userValue<randomNumber){
+//     console.log("up")
+//   }else if(userValue>randomNumber){
+//     console.log("down")
+//   }else{
+//     console.log("정답입니다")
+//   }
+// }
+
+// play()
+
+//(2트)
+// let randomNumber = 0 ; //1.랜덤번호 지정
+
+// let userNumber = document.getElementById("number-area") //2.유저가 입력했을 때 아이디값 변수지정 
+// let buttonGo = document.getElementById("btn-go") //4.버튼에 변수할당
+// //5.클릭이벤트 넣어줘야함 ㅇㄷ에? ㅋ 
+// buttonGo.addEventListener("click",play)
+// function countNumber(){ //3. 랜덤번호 나올 함수 지정 
+//   randomNumber = Math.floor(Math.random()*100)+1;
+//   console.log("정답은",randomNumber);
+// }
+
+// function play(){
+//   let userValue = userNumber.value;
+//   console.log(userValue)
+//   if(userValue<randomNumber){
+//     console.log("up")
+//   } else if(userValue>randomNumber){
+//     console.log("down")
+//   } else {
+//     console.log("정답")
+//   }
+// }
+
+// countNumber() //4. 함수 호출 . 자꾸 빼먹음 ㅠ 
+// play()
+
+// (1번째 연습)
+// let randomNumber = 0; //0.랜덤번호 지정 
+// let buttonGo = document.getElementById("btn-go")
+// let enterNumber =document.getElementById("number-area")
+// buttonGo.addEventListener("click",play)
+
+// let resultArea = document.getElementById("result-area")
+
+
+// function randomGame(){
+//   randomNumber = Math.floor(Math.random()*100)+1;
+//   console.log("정답",randomNumber)
+// }
+
+// function play(){//인풋에 입력한 값을 불러오는 함수  .
+//   let userValue = enterNumber.value;
+//   console.log(userValue)
+  
+//   if(userValue>randomNumber){
+//     resultArea.textContent = "down!"
+//   } else if(userValue<randomNumber){
+//     resultArea.textContent= "up!"
+//   }else {
+//     resultArea.textContent= "정답입니다!"
+//   }
+// }
+
+
+
+// randomGame()
+// play()
+
+/**-----------------------------------------(정답) */
+
+// let computerNum = 0;
+// let playButton = document.getElementById("btn-play") 
+// let userInput = document.getElementById("user-input")
+// let resultArea = document.getElementById("result-area")//결과값 가져올 div 가져와줌 
+// playButton.addEventListener("click",play) // play()라는 함수를 매개변수로 넘겨줌 
+
+// function pickRandomNum(){
+//   computerNum = Math.floor( Math.random()*100)+1;
+//   console.log("정답",computerNum)
+// }
+
+// function play(){//유저가 입력한 값을 알아야 비교할 수 있음 //input 값 가져오기 
+//  let userValue =  userInput.value;
+//  console.log(userValue);
+//  //4.랜덤 번호 < 유저번호 "Down!,  5.랜덤 번호 > 유저번호 "Up!"
+//  if(userValue < computerNum){
+//   resultArea.textContent = "UP!";
+//  } else if(userValue > computerNum){
+//   resultArea.textContent = "DOWN!";
+//  } else {
+//   resultArea.textContent = "맞췄습니다!";D
+//  }
+ 
+// }
+// pickRandomNum()
+
